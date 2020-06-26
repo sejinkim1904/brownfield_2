bfd778434be0ff63984aa7872b4f1c8a406edb2b
Sejin Kim <sejin.kim1904@gmail.com>
2020-06-26 12:16:54 -0600

    Add user to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -116,3 +116,4 @@ m3_tutorial.videos.create!({
 })
 
 User.create!(email: 'admin@example.com', first_name: 'Bossy', last_name: 'McBosserton', password:  "password", role: :admin)
+User.create!(email: 'sejin@example.com', first_name: 'Sejin', last_name: 'Kim', password:  "password", token: ENV['GITHUB_TOKEN'])
